33f2784390bce37ce5971d841ab127599a724671
Ankur Seth <iankurseth@gmail.com>
2022-02-09 02:41:03 +0530

    cra, tailwind and cleanup

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# `iaseth.github.io`
+
+
---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+
+function App () {
+	return (
+		<div className="App">
+			<div className="min-h-screen flex bg-red-400 text-white">
+				<div className="text-4xl m-auto">
+					<a href="https://github.com/iaseth" className="bg-red-500 px-6 py-3 duration-300 hover:bg-red-600">iaseth</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
